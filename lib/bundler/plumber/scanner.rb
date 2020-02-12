558309b38090418e37582c3acebbb8b72c1a5bfa
bronzdoc <lsagastume1990@gmail.com>
2020-02-12 13:05:58 -0600

    Enable ignore logic in the scanner

--- lib/bundler/plumber/scanner.rb
+++ lib/bundler/plumber/scanner.rb
@@ -118,12 +118,10 @@ module Bundler
 
         @lockfile.specs.each do |gem|
           @database.check_gem(gem) do |advisory|
-
-            # TODO this logic should be modified for rubymem
-            #unless (ignore.include?(advisory.cve_id) || ignore.include?(advisory.osvdb_id))
-            #  yield UnpatchedGem.new(gem,advisory)
-            #end
-            yield UnpatchedGem.new(gem, advisory)
+            gem_and_id = "#{advisory.gem}-#{advisory.id}"
+            unless ignore.include?(gem_and_id)
+              yield UnpatchedGem.new(gem,advisory)
+            end
           end
         end
       end
